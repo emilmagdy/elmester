51851ae083c507798c472ec362f912da7bd3ff9a
emilmagdy <dremilmagdy@gmail.com>
2026-06-13 01:01:24 +0300

    adding the error handler middleware and editng the routes to the handler

--- server.js
+++ server.js
@@ -9,25 +9,25 @@ const PORT = process.env.PORT || 3000;
 
 // Connect to the cloud database (Neon) using the connection string
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Your Neon connection string
-  ssl: {
-    rejectUnauthorized: false // Required for secure Neon cloud connections
-  },
-  // 1. Connection Timeout: Time to wait for a new connection before throwing an error (set to 10 seconds)
-  connectionTimeoutMillis: 10000, 
-  
-  // 2. Idle Timeout: Time a client can sit idle in the pool before being closed (set to 30 seconds)
-  idleTimeoutMillis: 30000, 
-  
-  // 3. Max Clients: Maximum number of clients allowed inside the pool simultaneously
-  max: 10 
+    connectionString: process.env.DATABASE_URL, // Your Neon connection string
+    ssl: {
+        rejectUnauthorized: false // Required for secure Neon cloud connections
+    },
+    // 1. Connection Timeout: Time to wait for a new connection before throwing an error (set to 10 seconds)
+    connectionTimeoutMillis: 10000,
+
+    // 2. Idle Timeout: Time a client can sit idle in the pool before being closed (set to 30 seconds)
+    idleTimeoutMillis: 30000,
+
+    // 3. Max Clients: Maximum number of clients allowed inside the pool simultaneously
+    max: 10
 });
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
 // Middleware to parse the URL Encoded data embedded in the form body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Configure session middleware to track logged-in users
 app.use(session({
@@ -58,13 +58,13 @@ const requireAuth = (req, res, next) => {
 // ===================
 app.get('/', (req, res) => {
     // Renders index.ejs which contains navigation buttons and platform features
-    res.render('index'); 
+    res.render('index');
 });
 
 // ==========================================
 // 2. Teachers List Route
 // ==========================================
-app.get('/teachers',  async (req, res) => {
+app.get('/teachers', async (req, res) => {
     const { subject, sort } = req.query;
     try {
         let queryText = `
@@ -74,7 +74,7 @@ app.get('/teachers',  async (req, res) => {
             FROM teachers t
             LEFT JOIN reviews r ON t.id = r.teacher_id AND r.status = 'approved'
             `;
-        
+
         const queryParams = []
 
         if (subject && subject !== "all") {
@@ -91,28 +91,27 @@ app.get('/teachers',  async (req, res) => {
             queryText += " ORDER BY t.name ASC";
         };
 
-        const [result,subjectRows ]= await Promise.all([
-            pool.query(queryText,queryParams),
+        const [result, subjectRows] = await Promise.all([
+            pool.query(queryText, queryParams),
             pool.query("SELECT DISTINCT subject FROM teachers ORDER BY subject")]);
         const subjectList = subjectRows.rows.map(row => row.subject);
 
-        // Pass the database rows to teachers.ejs view
         res.render('teachers', {
-             teachers: result.rows,
-             selectedSort: sort || "default",
-             selectedSubject: subject || "all",
-             subjects : subjectList
-             }); 
+            teachers: result.rows,
+            selectedSort: sort || "default",
+            selectedSubject: subject || "all",
+            subjects: subjectList
+        });
     } catch (err) {
-        console.error("Database fetch error:", err);
-        res.status(500).send("Server Error: Status 500 - Failed to load teachers list");
+        next(err)
     }
 });
+
 // ================================================================
 // GET Route to render the form that will be used to input the data
 // ================================================================
 
-app.get("/admin-insert", (req,res) => {
+app.get("/admin-insert", (req, res) => {
     // Get the secret key from the URL Parameters
     const secretKey = req.query.secret;
     //Validate the secret Key
@@ -127,31 +126,31 @@ app.get("/admin-insert", (req,res) => {
 // Post Route : To send the form data into the database
 // ====================================================
 
-app.post("/admin-insert" , async (req, res) => {
+app.post("/admin-insert", async (req, res) => {
     // Get the secret key from the URL Parameters
     const secretKey = req.query.secret;
     // Double Check to prevent direct attacks
     if (secretKey !== "EmilPassword123") {
-       return  res.status(403).send("Access Denied : Unautherized entry")
+        return res.status(403).send("Access Denied : Unautherized entry")
     };
     try {
         // Destructure Name and Subject constants from the form body
-        const {name , subject } = req.body;
+        const { name, subject } = req.body;
         // Insert name and subject into the database
         const queryText = "INSERT INTO teachers (name,subject) VALUES ($1, $2)"
-        await pool.query(queryText, [name,subject]);
+        await pool.query(queryText, [name, subject]);
         // Success : Redirect to hte homepage
         res.redirect("/")
     } catch (err) {
-        console.error("Data insertion error", err);
-        res.status(500).send("Server Error : Failed to save data")
-    }});
+        next(err)
+    }
+});
 
 // ===========================================
 // GET Route for renderign the register page
 // ===========================================
 
-app.get("/register", (req,res) => {
+app.get("/register", (req, res) => {
     res.render("register")
 });
 
@@ -159,35 +158,33 @@ app.get("/register", (req,res) => {
 // POST Route for saving the registrion data into the database
 // ===========================================================
 
-app.post("/register", async (req,res) =>{
-    let {name, email , password ,grade } = req.body;
-try{
-    email = email.trim().toLowerCase()
-    const exits = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
-    if (exits.rows.length > 0) {
-        return res.status(400).send("This email is already registered. Please log in ")
-    }
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const newUser = await pool.query(
+app.post("/register", async (req, res) => {
+    let { name, email, password, grade } = req.body;
+    try {
+        email = email.trim().toLowerCase()
+        const exits = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (exits.rows.length > 0) {
+            return res.status(400).send("This email is already registered. Please log in ")
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = await pool.query(
             'INSERT INTO users (name, email, password_hash, grade) VALUES ($1, $2, $3, $4) RETURNING id, role',
             [name, email, hashedPassword, grade]
         );
-     req.session.userId = newUser.rows[0].id;
-     req.session.userRole = newUser.rows[0].role;
-     req.session.userName = newUser.rows[0].name;
-     req.session.save((err) => {
+        req.session.userId = newUser.rows[0].id;
+        req.session.userRole = newUser.rows[0].role;
+        req.session.userName = newUser.rows[0].name;
+        req.session.save((err) => {
             if (err) {
                 console.error("Session save error during registration:", err);
                 return res.status(500).send("Internal Server Error");
             }
             res.redirect("/teachers");
         });
-
     } catch (err) {
-        console.error("Error during signup registration:", err);
-        res.status(500).send("Internal server error. Please try again later.");
+        next(err)
     }
-    });
+});
 
 // ======================================
 // GET Route for rendering the login page
@@ -200,24 +197,24 @@ app.get("/login", (req, res) => {
 // ===========================================
 // POST Route for sending the login credntials
 // ===========================================
- app.post("/login", async (req, res) => {
-    let {email, password} = req.body;
+app.post("/login", async (req, res) => {
+    let { email, password } = req.body;
     email = email.trim().toLowerCase()
     try {
         const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
         if (result.rows.length === 0) {
-        return res.status(400).send("Invalid Email or Password");
-    };
-    const user = result.rows[0];
-    const isMatch = await bcrypt.compare(password, user.password_hash);
-    if (!isMatch) {
-        return res.status(400).send("Invalis Email or Password");
-    }
+            return res.status(400).send("Invalid Email or Password");
+        };
+        const user = result.rows[0];
+        const isMatch = await bcrypt.compare(password, user.password_hash);
+        if (!isMatch) {
+            return res.status(400).send("Invalis Email or Password");
+        }
 
-    req.session.userId = user.id;
-    req.session.userRole = user.role;
-    req.session.userName = user.name;
-    req.session.save((err) => {
+        req.session.userId = user.id;
+        req.session.userRole = user.role;
+        req.session.userName = user.name;
+        req.session.save((err) => {
             if (err) {
                 console.error("Session save error during login:", err);
                 return res.status(500).send("Internal Server Error");
@@ -225,8 +222,7 @@ app.get("/login", (req, res) => {
             res.redirect("/teachers")
         });
     } catch (err) {
-        console.error("Error during login", err);
-        res.status(500).send("Internal Server error. Please try again later");
+        next(err)
     }
 });
 
@@ -237,8 +233,7 @@ app.get("/logout", (req, res) => {
     // Destroy the session in the server and clear the browser cookie
     req.session.destroy((err) => {
         if (err) {
-            console.error("Error destroying session during logout:", err);
-            return res.status(500).send("Failed to log out smoothly.");
+            return next(err)
         }
         res.clearCookie("connect.sid"); // Clear the default express-session cookie ID
         res.redirect("/"); // Redirect back to the universal landing homepage
@@ -249,23 +244,22 @@ app.get("/logout", (req, res) => {
 // POST route to send the review into the reviews tabke in the database
 // =====================================================================
 
-app.post("/teachers/:id/review" ,requireAuth, async (req, res) => {
+app.post("/teachers/:id/review", requireAuth, async (req, res) => {
     const teacher_id = req.params.id;
     const student_id = req.session.userId;
     const { rating, review_text } = req.body;
-    
+
     try {
         const checkQuery = "SELECT * FROM reviews WHERE teacher_id=$1 AND student_id = $2";
-        const existingReview = await pool.query(checkQuery, [teacher_id,student_id]);
+        const existingReview = await pool.query(checkQuery, [teacher_id, student_id]);
         if (existingReview.rows.length > 0) {
-           return  res.status(400).send("لا يمكن ارسال اكثر من تقييم واحد لكل مدرس")
-        } 
+            return res.status(400).send("لا يمكن ارسال اكثر من تقييم واحد لكل مدرس")
+        }
         const queryText = "INSERT INTO reviews (teacher_id, student_id, rating , review_text) VALUES ($1, $2, $3, $4)";
-        const review_entry = await pool.query(queryText, [teacher_id, student_id,rating,review_text]);
+        const review_entry = await pool.query(queryText, [teacher_id, student_id, rating, review_text]);
         res.redirect("/teachers");
-    } catch (err){
-        console.error(err);
-        res.status(400).send("Error during saving review");
+    } catch (err) {
+          next(err)
     }
 });
 
@@ -275,7 +269,7 @@ app.post("/teachers/:id/review" ,requireAuth, async (req, res) => {
 
 app.get("/teachers/:id", async (req, res) => {
     const teacher_id = req.params.id;
-    
+
     try {
         const teacherQuery = "SELECT * FROM teachers WHERE id = $1";
         const reviewsQuery = `
@@ -285,21 +279,47 @@ app.get("/teachers/:id", async (req, res) => {
         const [teacherResult, reviewsResult] = await Promise.all([
             pool.query(teacherQuery, [teacher_id]),
             pool.query(reviewsQuery, [teacher_id])]);
-        
+
         if (teacherResult.rows.length === 0) {
             return res.status(404).send("المدرس غير موجود");
         }
 
-        res.render("teacher-reviews", { 
-            teacher: teacherResult.rows[0], 
-            reviews: reviewsResult.rows       
+        res.render("teacher-reviews", {
+            teacher: teacherResult.rows[0],
+            reviews: reviewsResult.rows
         });
 
     } catch (err) {
-        console.error(err);
-        res.status(500).send("Error fetching teacher details and reviews");
+        next(err)
     }
 });
+
+app.use((req, res, next) => {
+    const err = new Error("هذه الصفحه غير موجوده او تم حذفها");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.status || 500;
+    if (statusCode === 500) {
+        console.error(`[SERVER ERROR] [${new Date().toISOString()}]:`, err.stack);
+    }
+    let userMessage = " عذرا ... حدث خطا فى السيرفر و جارى العمل على اصلاحه فى اسرع وقت";
+    if (statusCode === 404) {
+        userMessage = err.message;
+    } else if (err.code === "23505") {
+        userMessage = "لقد قمت بكتابة تقييم عن هذا المدرس مسبقا"
+    }
+
+    res.status(statusCode).render("error", {
+        title: statusCode === 404 ? "هذه الصفحه ليست موجوده" : "خطأ فى السيرفر",
+        message: userMessage,
+        status: statusCode
+    });
+})
+
+
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
