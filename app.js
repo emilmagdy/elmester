b8489ee44fecefca92b93972cf1ab98e378e0b6d
emilmagdy <dremilmagdy@gmail.com>
2026-07-08 00:35:21 +0300

    debugging the error during sending mail

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
-
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const session = require("express-session")
 const express = require('express');
 const app = express();
